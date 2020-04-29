62b9520897be3f08ee435e97a6dda0dee6eb9375
Eugene Shishkin <eshishkin@wiley.com>
2020-04-29 19:27:09 +0400

    https://github.com/eshishkin/library-manager/issues/7: initial commit with simple Spring Boot application

---
+++ library-manager-portal/src/main/java/org/eshishkin/edu/librarymanager/portal/MainView.java
@@ -0,0 +1,16 @@
+package org.eshishkin.edu.librarymanager.portal;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Text("Welcome to MainView."));
+    }
+
+}
+
+
